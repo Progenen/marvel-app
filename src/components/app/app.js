29425c746d54d1555cc12100c51965df1f4c92d5
John Doe <stanislav_2010@icloud.com>
2022-01-03 02:21:46 +0500

    Init charArticle component and adding in app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,14 @@
 // import Char from "../char/char";
 // import CatalogComics from "../catalogComics/catalogComics";
-import ComicsArticle from "../comicsArticle/comicsArticle";
+// import ComicsArticle from "../comicsArticle/comicsArticle";
+import CharArtilce from "../charArticle/charArticle";
 import Header from "../header/header";
 
 function App() {
     return (
         <>
             <Header/>
-            <ComicsArticle/>
+            <CharArtilce/>
         </>
     );
 }
---
+++ src/components/charArticle/charArticle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import ComicsBanner from "../comicsBanner/comicsBanner";
+
+export default function CharArtilce () {
+    return (
+        <>
+            <ComicsBanner></ComicsBanner>
+        </>
+    )
+}
